21581a7155fd12ac2fdbdf2f8b86762b158a334e
HaiNam102 <bimhuongday@gmail.com>
2024-12-09 20:03:11 +0700

    update

--- src/Component/Admin/Admin.js
+++ src/Component/Admin/Admin.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import SideBar from './SideBar';
-import NavBar from './NavBar';
 import { Outlet } from "react-router-dom";
 import './Admin.scss';
 import { ToastContainer } from 'react-toastify';
